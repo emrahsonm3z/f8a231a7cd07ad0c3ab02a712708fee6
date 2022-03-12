94b0b0089b7ea38e2ace9b4af7a70ab22a556b92
emrahsonm3z <emrah.sonmez@itsbilisim.com>
2022-03-13 01:52:24 +0300

    Created product service.

--- services/product.service.ts
+++ services/product.service.ts
@@ -1,11 +1,10 @@
-import { Product } from '~/container/product/types/Product'
 import { request } from '~/utils/request'
 
 interface IProductService {
-  getAll(params?: Record<string, any>): Promise<Product[]>
+  getAll(params?: Record<string, any>): Promise<any>
 }
 
-const getAll = async (params?: Record<string, any>): Promise<Product[]> =>
+const getAll = async (params?: Record<string, any>): Promise<any> =>
   request.get('/products', { params })
 
 export const productService: IProductService = {
--- utils/request.ts
+++ utils/request.ts
@@ -1,13 +1,42 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
-import queryString from 'query-string'
 
 import { ACCESS_TOKEN, API_URL } from '~/constants'
 
-enum StatusCode {
-  Unauthorized = 401,
-  Forbidden = 403,
-  TooManyRequests = 429,
-  InternalServerError = 500
+const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+  let newConfig: AxiosRequestConfig = { ...config }
+  const token = ACCESS_TOKEN
+
+  if (token != null) {
+    newConfig = {
+      ...newConfig,
+      headers: {
+        ...config.headers,
+        'X-Access-Token': token
+      }
+    }
+  }
+  return newConfig
+}
+
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  console.error(`[request error] [${JSON.stringify(error)}]`)
+  return Promise.reject(error)
+}
+
+const onResponse = (response: AxiosResponse): any => {
+  console.debug('[response successfully]', response)
+  return response.status === 200 ? response.data : []
+}
+
+const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+  console.error(`[response error] [${JSON.stringify(error)}]`)
+  return Promise.reject(error)
+}
+
+function setupInterceptorsTo(axiosInstance: AxiosInstance): AxiosInstance {
+  axiosInstance.interceptors.request.use(onRequest, onRequestError)
+  axiosInstance.interceptors.response.use(onResponse, onResponseError)
+  return axiosInstance
 }
 
 const headers: Readonly<Record<string, string | boolean>> = {
@@ -17,111 +46,9 @@ const headers: Readonly<Record<string, string | boolean>> = {
   'X-Requested-With': 'XMLHttpRequest'
 }
 
-// We can use the following function to inject the JWT token through an interceptor
-const injectToken = (config: AxiosRequestConfig): AxiosRequestConfig => {
-  let newConfig: AxiosRequestConfig = { ...config }
-  try {
-    const token = ACCESS_TOKEN
-
-    if (token != null) {
-      newConfig = {
-        ...newConfig,
-        headers: {
-          ...config.headers,
-          'X-Access-Token': token
-        }
-      }
-    }
-    return newConfig
-  } catch (error) {
-    throw new Error('ACCESS TOKEN NOT FOUND')
-  }
-}
-
-class Http {
-  private instance: AxiosInstance | null = null
-
-  private get http(): AxiosInstance {
-    return this.instance != null ? this.instance : this.initHttp()
-  }
-
-  initHttp() {
-    const http = axios.create({
-      baseURL: API_URL,
-      headers
-    })
-
-    http.interceptors.request.use(injectToken, (error) => Promise.reject(error))
-
-    http.interceptors.response.use(
-      (response: AxiosResponse) => response,
-      (error: AxiosError) => {
-        const { response } = error
-        return this.handleError(response)
-      }
-    )
-
-    this.instance = http
-    return http
-  }
-
-  request<T = any, R = AxiosResponse<T>>(config: AxiosRequestConfig): Promise<R> {
-    return this.http.request(config)
-  }
-
-  get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
-    let newConfig: AxiosRequestConfig = { ...config }
-
-    if (typeof config.params !== 'undefined') {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const { params, ...otherOptions } = config
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      const query = queryString.stringify(params, {
-        arrayFormat: 'index'
-      })
-
-      newConfig = {
-        ...otherOptions,
-        url: `${config.url}?${query}`
-      }
-    }
-
-    return this.http.get<T, R>(url, newConfig)
-  }
-
-  // Handle global app errors
-  // We can handle generic app errors depending on the status code
-  // eslint-disable-next-line class-methods-use-this
-  private handleError(error) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { status } = error
-
-    switch (status) {
-      case StatusCode.InternalServerError: {
-        // Handle InternalServerError
-        break
-      }
-      case StatusCode.Forbidden: {
-        // Handle Forbidden
-        break
-      }
-      case StatusCode.Unauthorized: {
-        // Handle Unauthorized
-        break
-      }
-      case StatusCode.TooManyRequests: {
-        // Handle TooManyRequests
-        break
-      }
-
-      default: {
-        // Default
-        break
-      }
-    }
-
-    return Promise.reject(error)
-  }
-}
-
-export const request = new Http()
+export const request = setupInterceptorsTo(
+  axios.create({
+    baseURL: API_URL,
+    headers
+  })
+)
